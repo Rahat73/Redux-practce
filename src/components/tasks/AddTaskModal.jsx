a1b4d77e417069bace63a52355010a6970f8e4f0
Rahat73 <rahat.ashik.18@gmail.com>
2024-02-21 19:42:05 +0600

    add task form

--- src/components/tasks/AddTaskModal.jsx
+++ src/components/tasks/AddTaskModal.jsx
@@ -1,12 +1,86 @@
+import { useForm } from "react-hook-form";
 import MyModal from "../../ui/Modal";
 
 const AddTaskModal = ({ isOpen, setIsOpen }) => {
+  const { register, handleSubmit, reset } = useForm();
+
+  const onCancel = () => {
+    reset();
+    setIsOpen(false);
+  };
+
+  const onSubmit = (data) => {
+    onCancel();
+    console.log(data);
+  };
   return (
     <MyModal isOpen={isOpen} setIsOpen={setIsOpen} title={"Add Task"}>
-      <p className="text-sm text-gray-500">
-        Your payment has been successfully submitted. We’ve sent you an email
-        with all of the details of your order.
-      </p>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex flex-col mb-5">
+          <label htmlFor="title">Title</label>
+          <input
+            id="title"
+            type="text"
+            {...register("name")}
+            className="rounded-lg"
+          />
+        </div>
+        <div className="flex flex-col mb-5">
+          <label htmlFor="description">Description</label>
+          <textarea
+            id="description"
+            type="text"
+            {...register("description")}
+            className="rounded-lg"
+          />
+        </div>
+        <div className="flex flex-col mb-5">
+          <label htmlFor="date">Date</label>
+          <input
+            id="date"
+            type="date"
+            {...register("date")}
+            className="rounded-lg"
+          />
+        </div>
+        <div className="flex flex-col mb-5">
+          <label htmlFor="assignedTo">Assign to</label>
+          <select
+            id="assignedTo"
+            {...register("assignedTo")}
+            className="w-full rounded-lg"
+          >
+            <option value={"Shakib"}>Shakib</option>
+            <option value={"Tamim"}>Tamim</option>
+            <option value={"Mushi"}>Mushi</option>
+            <option value={"Riyad"}>Riyad</option>
+            <option value={"Fizz"}>Fizz</option>
+            <option value={"Litton"}>Litton</option>
+          </select>
+        </div>
+        <div className="flex flex-col mb-5">
+          <label htmlFor="priority">Assign to</label>
+          <select
+            id="priority"
+            {...register("priority")}
+            className="w-full rounded-lg"
+          >
+            <option defaultValue value={"high"}>
+              High
+            </option>
+            <option value={"medium"}>Medium</option>
+            <option value={"low"}>Low</option>
+          </select>
+        </div>
+        <div className="flex justify-end gap-3">
+          <button onClick={onCancel} type="button" className="btn btn-danger">
+            Cancel
+          </button>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+      </form>
     </MyModal>
   );
 };
