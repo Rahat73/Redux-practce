f38853acb497f2c336d86221198fcc04fc71da83
Rahat73 <rahat.ashik.18@gmail.com>
2024-02-29 15:01:27 +0600

    fix

--- src/components/tasks/TaskDetailsModal.jsx
+++ src/components/tasks/TaskDetailsModal.jsx
@@ -4,7 +4,7 @@ import Modal from "../ui/Modal";
 const TaskDetailsModal = ({ isOpen, setIsOpen, id }) => {
   const { data: tasks } = useGetTasksQuery();
 
-  const task = tasks.find((item) => item.id === id);
+  const task = tasks?.find((item) => item.id === id);
 
   return (
     <Modal isOpen={isOpen} setIsOpen={setIsOpen} title={task?.title}>
